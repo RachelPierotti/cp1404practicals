a828ddb5847a26cc4948e1640f2ae89bf74f1944
Rachel <rachel.pierotti@my.jcu.edu.au>
2023-10-02 16:35:28 +1000

    Avoid ZeroDivision Error in program

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -9,6 +9,9 @@ Answer the following questions:
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("Please enter a non-zero integer!")
+        denominator = int(input("Enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
@@ -16,3 +19,8 @@ except ValueError:
 except ZeroDivisionError:
     print("Cannot divide by zero!")
 print("Finished.")
+
+# Q1: A ValueError will occur when a non-integer number is entered, or other symbol e.g. "a" is entered.
+# Q2: A ZeroDivisionError will occur when the denominator is entered as zero.
+# Q3: Add a loop so that if zero is the denominator input, the program asks the user for a different value before
+# evaluating the fraction.
