039da88bd7b56173f9741a2056310844b77c7ed9
Rachel <rachel.pierotti@my.jcu.edu.au>
2023-10-02 16:37:36 +1000

    Add exceptions_to_complete code

---
+++ prac_03/exceptions_to_complete.py
@@ -0,0 +1,13 @@
+"""
+CP1404 - Prac 3
+Fill in the TODOs to complete the task
+"""
+
+is_finished = False
+while not is_finished:
+    try:
+        result = int(input("Enter a valid integer: "))
+        # TODO: this line
+    except:  # TODO - add the exception you want to catch after except
+        print("Please enter a valid integer.")
+print("Valid result is:", result)
