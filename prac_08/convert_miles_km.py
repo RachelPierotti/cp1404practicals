060a4696ed24df99322e0bdd5abfa1c5c12defe4
Rachel <rachel.pierotti@my.jcu.edu.au>
2023-11-12 23:09:58 +1000

    Add functionality to convert_miles_km program

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,13 +1,39 @@
-"""Convert Miles to Kilometers Program"""
+"""Convert Miles to Kilometres Program"""
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+MILES_TO_KM = 1.60934
 
 
-class BoxLayoutDemo(App):
+class MilesToKilometres(App):
+    "Convert miles to kilometres."
+
     def build(self):
-        self.title = "Box Layout Demo"
+        "Build app."
+        self.title = "Miles To Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
+    def handle_conversion(self):
+        """Conversion calculation: miles to kilometres"""
+        miles = self.get_valid_miles()
+        kilometres = miles * MILES_TO_KM
+        self.root.ids.output_kilometres.text = str(kilometres)
 
-BoxLayoutDemo().run()
+    def handle_increment(self, increment):
+        """Change miles value by a positive or negative increment of 1"""
+        miles = self.get_valid_miles() + increment
+        self.root.ids.input_miles.text = str(miles)
+        self.handle_conversion()
+
+    def get_valid_miles(self):
+        """Get a valid input"""
+        try:
+            miles = float(self.root.ids.input_miles.text)
+            return miles
+        except ValueError:
+            return 0.0
+
+
+MilesToKilometres().run()
