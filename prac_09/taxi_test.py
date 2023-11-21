df06916e932ae1cf26c753ecd3cf6265c992bb13
Rachel <rachel.pierotti@my.jcu.edu.au>
2023-11-21 12:15:05 +1000

    Create new taxi object

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,12 @@
+"""taxi_test program"""
+from prac_09.taxi import Taxi
+
+
+def main():
+    """Create a new taxi object"""
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    print(my_taxi)
+
+
+main()
+
