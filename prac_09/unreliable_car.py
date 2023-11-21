832b86a710ee52ca59fadb2cb78eb9f8038a51ba
Rachel <rachel.pierotti@my.jcu.edu.au>
2023-11-21 14:40:16 +1000

    Override drive method

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,5 +1,6 @@
 """Unreliable Car class"""
 from prac_09.car import Car
+from random import uniform
 
 
 class UnreliableCar(Car):
@@ -8,4 +9,14 @@ class UnreliableCar(Car):
     def __init__(self, reliability, **kwargs):
         """Initialise an UnreliableCar instance, based on parent class Car."""
         super().__init__(**kwargs)
-        self.reliability = reliability
+        self.reliability = float(reliability)
+
+    def drive(self, distance):
+        """Drive the car if reliable."""
+        random_number = uniform(0, 100)
+        if random_number >= self.reliability:
+            distance = 0
+        else:
+            distance = distance
+        distance_driven = super().drive(distance)
+        return distance_driven
