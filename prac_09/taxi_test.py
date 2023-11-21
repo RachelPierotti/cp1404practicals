9bb3b045f9163cc12363564c08e4c8bf931db6e9
Rachel <rachel.pierotti@my.jcu.edu.au>
2023-11-21 13:16:02 +1000

    Complete taxi_test program

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -3,10 +3,21 @@ from prac_09.taxi import Taxi
 
 
 def main():
-    """Create a new taxi object"""
+    """Create a new taxi object and drive it"""
     my_taxi = Taxi("Prius 1", 100, 1.23)
-    print(my_taxi)
+    Taxi.start_fare(my_taxi)
+    Taxi.drive(my_taxi, 40)
+    fare = Taxi.get_fare(my_taxi)
+    print(f"Taxi's details: {my_taxi}")
+    print(f"Current fare is: ${fare:.2f}")
+    Taxi.start_fare(my_taxi)
+    Taxi.drive(my_taxi, 100)
+    fare = Taxi.get_fare(my_taxi)
+    print(f"Taxi's details: {my_taxi}")
+    print(f"Current fare is: ${fare:.2f}")
+    # because of the nature of Car class, taxi stops driving after
+    # 60km as it runs out of fuel, so distance and fare is impacted
+    # by this attribute
 
 
 main()
-
