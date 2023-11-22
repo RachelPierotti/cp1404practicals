0b5c8d66a5953af0ff0568fc1f4a74412c7b74a0
Rachel <rachel.pierotti@my.jcu.edu.au>
2023-11-22 10:24:15 +1000

    Add flagfall class variable

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -2,7 +2,8 @@
 from prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
-    """Specialised version of Taxi includes fanciness factor"""
+    """Specialised version of Taxi that includes fanciness factor"""
+    flagfall = 4.50
     def __init__(self, name, fuel, fanciness):
         """Initialise a SilverServiceTaxi instance, based on parent class
         Taxi (which inherits from its parent class Car)."""
@@ -10,3 +11,10 @@ class SilverServiceTaxi(Taxi):
         self.fanciness = float(fanciness)
         self.price_per_km *= fanciness
 
+    def __str__(self):
+        """Return a string like Taxi with flagfall."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return (self.price_per_km * self.current_fare_distance) + self.flagfall
